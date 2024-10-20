902b86d908a91e51870bbaf2657b4196629d2378
MatthewJer <matthias-j@hotmail.de>
2024-10-20 21:12:38 +0200

    implemented decoding

--- MainClass.java
+++ MainClass.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 public class MainClass {
 
-    public static List<byte[]> chunkByThree(byte[] inputArray) {
+    private static final String base64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    private static List<byte[]> chunkByThree(byte[] inputArray) {
         List<byte[]> byteChunks = new ArrayList<>();
         for (int i = 0; i < inputArray.length; i += 3) {
             int remainingBytes = inputArray.length - i;
@@ -17,11 +19,20 @@ public class MainClass {
         return byteChunks;
     }
 
-    public static void main (String [] args)
+    private static List<char[]> chunkByFour(char[] inputArray) {
+        List<char[]> byteChunks = new ArrayList<>();
+        for (int i = 0; i < inputArray.length; i += 4) {
+            int remainingChars = inputArray.length - i;
+            int currentChunkSize = Math.min(remainingChars, 4);
+            char[] chunk = new char[currentChunkSize];
+            System.arraycopy(inputArray, i, chunk, 0, currentChunkSize);
+            byteChunks.add(chunk);
+        }
+        return byteChunks;
+    }
+
+    private static String encode(String input)
     {
-        Console console = System.console(); 
-        String base64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-        String input = console.readLine();
         var bytes = input.getBytes();
         var chunkedByThree = chunkByThree (bytes);
         String result = "";
@@ -42,6 +53,60 @@ public class MainClass {
                       Character.toString(thirdChar) +
                       Character.toString(fourthChar);
         }
+        return result;
+    }
+
+    private static String decode(String input)
+    {
+        var chunkedByFour = chunkByFour(input.toCharArray());
+        byte [] bytes = new byte[input.length() / 4 * 3];
+        int startIndex = 0;
+        boolean lasTwoMissing = false;
+        boolean lastOneMissing = false;
+        for (var chunk : chunkedByFour) {
+            byte firstByte;
+            int firstCharIndex = base64Chars.indexOf(chunk[0]);
+            firstByte = (byte)(firstCharIndex << 2);
+            int secondCharIndex = base64Chars.indexOf(chunk[1]);
+            firstByte += (secondCharIndex & 0b00110000) >> 4;
+            byte secondByte = (byte)((secondCharIndex & 0b00001111) << 4);
+            byte thirdByte = 0;
+            if (chunk.length > 2 && chunk[3] != '=') {
+                int thirdCharIndex = base64Chars.indexOf(chunk[2]);
+                secondByte += (byte)((thirdCharIndex & 0b00111100) >> 2);
+                if (chunk.length > 3 && chunk[3] != '=') {
+                    thirdByte = (byte)((thirdCharIndex & 0b00000011) << 6);
+                    thirdByte += base64Chars.indexOf(chunk[3]);
+                } else {
+                    lastOneMissing = true;
+                }
+            } else {
+                lasTwoMissing = true;
+            }
+            bytes[startIndex] = firstByte;
+            bytes[startIndex + 1] = secondByte;
+            bytes[startIndex + 2] = thirdByte;
+            startIndex += 3;
+        }
+        if (lasTwoMissing || lastOneMissing) {
+            var subArray = new byte[bytes.length - (lasTwoMissing ? 2 : 1)];
+            System.arraycopy(bytes, 0, subArray, 0, subArray.length);
+            return new String(subArray);
+        }
+        return new String(bytes);
+    } 
+
+    public static void main (String [] args)
+    {
+        Console console = System.console();
+        if (args.length != 2 || !(args[0].equals("encode") || args[0].equals("decode"))) {
+            console.println("Usage: <encode/decode> <input>");
+        }
+        String result = null;
+        if (args[0].equals("encode"))
+            result = encode(args[1]);
+        else
+            result = decode(args[1]);
         console.println(result);
     }
 
